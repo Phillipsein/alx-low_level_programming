ec118b1d9ff7619397585f4c049fcef89eb08069
Phillipsein <phillipsein6@gmail.com>
2021-06-28 11:04:39 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Phillip Ssempereza
+ */
+
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of.
+ *
+ * Return: The length of @str.
+ */
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
+
